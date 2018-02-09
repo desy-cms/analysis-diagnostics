c6a9110af6f08ef750413bd9d66214c649e0ade6
Roberval Walsh <roberval.walsh@gmail.com>
2018-02-09 12:13:39 +0100

    updates

--- plugins/TriggerInfo.cc
+++ plugins/TriggerInfo.cc
@@ -25,7 +25,7 @@
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -49,7 +49,7 @@
 // constructor "usesResource("TFileService");"
 // This will improve performance in multithreaded jobs.
 
-class TriggerInfo : public edm::one::EDAnalyzer<>
+class TriggerInfo : public edm::EDAnalyzer
 {
    public:
       explicit TriggerInfo(const edm::ParameterSet&);
